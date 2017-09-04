eb5a25fd7f3efc66c376f324eed97d99e3947e16
Jesse Geerts <jesse.geerts.14@ucl.ac.uk>
2017-09-04 13:30:58 +0100

    Added colour gradient to 3d plotting module

--- plot_3d_trajectory.py
+++ plot_3d_trajectory.py
@@ -3,16 +3,45 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 import load_raw_data
-import seaborn as sns
 
 
-def update_lines(num, data, line):
-    line.set_data(data[0:2, :num])
-    line.set_3d_properties(data[2, :num])
+def set_3d_axes(labels=['X', 'Y', 'Z'], xlim=[-1.0, 1.0], ylim=[-1, 1], zlim=[0, 2.0]):
+    """
+    Creates a 3d axis
+
+    :param labels:
+    :param xlim:
+    :param ylim:
+    :param zlim:
+    :return: matplotlib figure and axis objects
+    """
+
+    # Create figure
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    # Setting the axes properties (note: reverse X and Y axes)
+    ax.set_xlim3d(xlim)
+    ax.set_xlabel(labels[0])
+
+    ax.set_ylim3d(ylim)
+    ax.set_ylabel(labels[1])
+
+    ax.set_zlim3d(zlim)
+    ax.set_zlabel(labels[2])
+
+    ax.set_title('3D trajectory')
+
+    return fig, ax
+
+
+def update_lines(num, data, line, points_drawn_per_frame):
+    line.set_data(data[0:2, :num * points_drawn_per_frame])
+    line.set_3d_properties(data[2, :num * points_drawn_per_frame])
     return line
 
 
-def plot_trajectory(filename, color_gradient=False, color_map='winter'):
+def plot_trajectory(df, color_gradient=False, color_map='winter'):
     """
 
     :param filename:
@@ -20,61 +49,41 @@ def plot_trajectory(filename, color_gradient=False, color_map='winter'):
     :param color_map:
     :return:
     """
-    # Load head position data
-    repo = load_raw_data.load_head_data(filename)
-    repo = repo[:20000]
-    data = np.array((repo['Value.M41'].values, repo['Value.M42'].values, repo['Value.M43'].values))
 
-    # Create figure
-    fig = plt.figure()
-    ax = p3.Axes3D(fig)
+    fig, ax = set_3d_axes()
+
+    # Load head position data
+    data = np.array((df['X'].values, df['Y'].values, df['Z'].values))
 
     if color_gradient is True:
         cm = plt.get_cmap(color_map)
-        ax.set_prop_cycle('color',[cm(1.*i/(data.shape[1]-1)) for i in range(data.shape[1]-1)])
-        for i in range(data.shape[1]-1):
-            ax.plot(data[0,i:i+2],data[2,i:i+2],data[1,i:i+2])
+        ax.set_prop_cycle('color', [cm(1. * i / (data.shape[1] - 1)) for i in range(data.shape[1] - 1)])
+        for i in range(data.shape[1] - 1):
+            ax.plot(data[0, i:i + 2], data[2, i:i + 2], data[1, i:i + 2])
     else:
         ax.plot(data[0, ], data[2, ], data[1, ])
 
-    # Setting the axes properties (note: reverse X and Y axes)
-    ax.set_xlim3d([-1.0, 1.0])
-    ax.set_xlabel('X')
 
-    ax.set_ylim3d([-1, 1])
-    ax.set_ylabel('Z')
+def plot_3d_video(df, points_drawn_per_frame=50, filename='trajectory'):
+    """
+    Plots a 3d video showing the trajectory of the head position in space.
 
-    ax.set_zlim3d([0, 2.0])
-    ax.set_zlabel('Y')
+    Usage:
+    data = get_location_dataframe(matrix_dataframe)
+    plot_3d_video(df)
 
-    ax.set_title('3D trajectory')
+    """
+    fig, ax = set_3d_axes()
+    # Load data into numpy array
+    data = np.array((df['X'].values, df['Y'].values, df['Z'].values))
+    line = ax.plot(data[0, 0:1], data[2, 0:1], data[1, 0:1])[0]  # Note: Z and Y axes are swapped here
 
+    ani = animation.FuncAnimation(fig,
+                                  update_lines,
+                                  int(data.shape[1] / points_drawn_per_frame),
+                                  fargs=(data, line, points_drawn_per_frame),
+                                  interval=1,
+                                  blit=False)
 
-def plot_3d_video():
-    # Attaching 3D axis to the figure
-    fig = plt.figure()
-    ax = p3.Axes3D(fig)
-
-    # Reading the data from a CSV file using pandas
-    repo = load_raw_data.load_head_data('./Data/head.csv')
-    repo = repo[:20000]
-
-    data = np.array((repo['Value.M41'].values, repo['Value.M42'].values, repo['Value.M43'].values))
-
-    line = ax.plot(data[0, 0:1], data[2, 0:1], data[1, 0:1])[0]
-
-    # Setting the axes properties
-    ax.set_xlim3d([-2.0, 2.0])
-    ax.set_xlabel('X')
-
-    ax.set_ylim3d([-2.0, 2.0])
-    ax.set_ylabel('Z')
-
-    ax.set_zlim3d([-2.0, 2.0])
-    ax.set_zlabel('Y')
-
-    ax.set_title('3D trajectory')
-
-    animation.FuncAnimation(fig, update_lines, data.shape[1], fargs=(data, line), interval=1, blit=False)
-
-    plt.show()
+    filename = filename + '.mp4' if filename[-4:] != '.mp4' else filename
+    ani.save(filename, fps=30, extra_args=['-vcodec', 'libx264'])
